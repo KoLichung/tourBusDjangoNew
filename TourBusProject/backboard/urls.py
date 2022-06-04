036b9f1a1b0721705dcac67217c1608bd88da99a
JasonKo <jason@kosbrother.com>
2022-06-04 15:51:09 +0800

    change login and logout method

--- TourBusProject/backboard/urls.py
+++ TourBusProject/backboard/urls.py
@@ -6,7 +6,8 @@ from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('base', views.base, name = 'base'), 
-    path('order_detail', views.order_detail, name = 'order_detail'), 
-    path('orders', views.orders, name = 'orders'), 
-    path('', include('django.contrib.auth.urls'))
+    path('order_detail', views.order_detail, name='order_detail'), 
+    path('orders', views.orders, name='orders'), 
+    path('', views.login, name='login'),
+    path('logout', views.logout, name='logout')
 ]
--- TourBusProject/backboard/views.py
+++ TourBusProject/backboard/views.py
@@ -3,25 +3,45 @@ from django.shortcuts import render ,redirect,get_object_or_404
 from django.http import HttpResponse
 import requests
 from tourBusCore.models import User ,Order
-import urllib 
-from django.db.models import Sum
-from datetime import datetime, timedelta
-
+from django.contrib import auth
+from django.contrib.auth import authenticate, logout
 # Create your views here.
 
 def base(request):
     return render(request, 'backboard/base.html')
 
-def orders(request):
-    q = request.GET.get('order_state')
+def login(request):
+    if request.method == 'POST':
+        phone = request.POST['phone']
+        password = request.POST['password']
+        user = authenticate(request, phone=phone, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/backboard/orders')
+        else:
+            return redirect('/backboard/')
 
-    orders = Order.objects.all()
+    return render(request, 'backboard/login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/backboard/')
+
+def orders(request):
+    if not request.user.is_authenticated or not request.user.is_staff:
+        return redirect('/backboard/')
+
+    q = request.GET.get('order_state')
+    orders = Order.objects.all().order_by('-id')
     if q!= None:
         orders = orders.filter(state=q)
  
     return render(request, 'backboard/orders.html',{'orders':orders, 'q':q})
 
 def order_detail(request):
+    if not request.user.is_authenticated or not request.user.is_staff:
+        return redirect('/backboard/')
+    
     order_id=request.GET.get('order_id')
     theOrder = Order.objects.get(id=order_id)
     
