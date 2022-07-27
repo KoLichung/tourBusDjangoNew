7b0eb2b725478cba52050f12d6c4157dde59c63c
JasonKo <jason@kosbrother.com>
2022-07-28 07:05:21 +0800

    change to psql

--- TourBusProject/app/settings.py
+++ TourBusProject/app/settings.py
@@ -114,11 +114,19 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql', 
+    #     'NAME': 'test_database',
+    #     'USER': 'test_user',
+    #     'PASSWORD': 'test_pass',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'test_database',
-        'USER': 'test_user',
-        'PASSWORD': 'test_pass',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME','mydatabase'),
+        'USER': os.environ.get('DB_USER','mydatabaseuser'),
+        'PASSWORD': os.environ.get('DB_PASS','mypassword'),
+        'HOST': os.environ.get('DB_HOST','127.0.0.1'),
+        'PORT': '5432',
     }
 }
 
