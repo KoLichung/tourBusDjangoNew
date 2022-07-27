196dfc0421e782da4b61cf7b0d34490793eef07c
JasonKo <jason@kosbrother.com>
2022-07-27 10:53:31 +0800

    change database

--- TourBusProject/app/settings.py
+++ TourBusProject/app/settings.py
@@ -110,9 +110,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'test_database',
+        'USER': 'test_user',
+        'PASSWORD': 'test_pass',
     }
 }
 
